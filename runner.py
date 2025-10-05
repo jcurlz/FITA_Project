296392b73c10fea0572f57ba3448bd9c5d7dbaa7
Anna Celesta <juditcurly@gmail.com>
2025-10-05 18:31:19 +0530

    fixed behave reports

--- runner.py
+++ runner.py
@@ -6,11 +6,15 @@ if __name__ == "__main__":
     features_path = os.path.join(os.getcwd(), "features")
     print(features_path)
     tags = sys.argv[1:] if len(sys.argv) > 1 else ['-t', 'widget']
+
+    report_path = os.environ.get("REPORT_PATH", "reports/behave_report.html")
+    os.makedirs(os.path.dirname(report_path), exist_ok=True)
+
     behave_args = [
         features_path,
         *tags,
         '--format', 'behave_html_formatter:HTMLFormatter',
-        '--out', 'reports/behave_report.html',
+        '--out', report_path,
         '--no-skipped',
         '--no-capture',
         '-f', 'plain'
