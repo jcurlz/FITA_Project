89ad2c2da89bd1f70f790981e20193c362fec114
Anna Celesta <juditcurly@gmail.com>
2025-10-05 18:40:23 +0530

    commented put behave args

--- runner.py
+++ runner.py
@@ -1,20 +1,16 @@
 import os
-import sys
+
 from behave.__main__ import main as behave_main
 
 if __name__ == "__main__":
     features_path = os.path.join(os.getcwd(), "features")
     print(features_path)
-    tags = sys.argv[1:] if len(sys.argv) > 1 else ['-t', 'widget']
-
-    report_path = os.environ.get("REPORT_PATH", "reports/behave_report.html")
-    os.makedirs(os.path.dirname(report_path), exist_ok=True)
 
     behave_args = [
         features_path,
-        *tags,
-        '--format', 'behave_html_formatter:HTMLFormatter',
-        '--out', report_path,
+        # '-t', 'widget',
+        # '--format', 'behave_html_formatter:HTMLFormatter',
+        # '--out', 'reports/behave_report.html',
         '--no-skipped',
         '--no-capture',
         '-f', 'plain'
